f4f66464dc50dba4a9e42412a31f55b790b5f4bf
a6delrahman <aabdelwa1@gmail.com>
2025-08-05 23:23:22 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,23 @@ FUNCTION_APP_NAME="splitpdfs-fapp-cont"
 STORAGE_ACCOUNT="splitpdfsa"
 APP_SERVICE_PLAN="splitpdfs-asp"
 LOCATION="switzerlandnorth"
+CLIENT_NAME="splitpdfs-client"
 
 # Login to Azure
 az login
 
 # Create resource group
 az group create --name $RESOURCE_GROUP --location "$LOCATION"
+
+# Assign contributor role to the current user logged in with az
+az role assignment create --assignee $(az ad signed-in-user show --query userPrincipalName --output tsv) --role Contributor --scope /subscriptions/$(az account show --query id --output tsv)/resourceGroups/$RESOURCE_GROUP
+
+# Create Azure AD application for the SharePoint Authentication
+az ad app create --display-name "$CLIENT_NAME"
+AD_APP_PRINCIPAL_ID=$(az ad app list --display-name $CLIENT_NAME --query "[0].appId" --output tsv)
+
+# Assign AD application the Storage Blob Data Contributor role
+az role assignment create --assignee $AD_APP_PRINCIPAL_ID --role "Storage Blob Data Contributor" --scope "/subscriptions/$(az account show --query id --output tsv)/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT"
 
 # Create ACR
 az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
@@ -45,5 +56,12 @@ az functionapp create \
     --registry-username $ACR_USERNAME \
     --registry-password $ACR_PASSWORD \
     --functions-version 4
+
+# Assign System Assigned Managed Identity to Function App
+az functionapp identity assign --name $FUNCTION_APP_NAME --resource-group $RESOURCE_GROUP
+
+# Assign Storage Blob Data Contributor role to the Function App's Managed Identity
+FUNCTION_APP_PRINCIPAL_ID=$(az functionapp identity show --name $FUNCTION_APP_NAME --resource-group $RESOURCE_GROUP --query principalId --output tsv)
+az role assignment create --assignee $FUNCTION_APP_PRINCIPAL_ID --role "Storage Blob Data Contributor" --scope /subscriptions/$(az account show --query id --output tsv)/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT
 
 echo "Deployment complete! Function App: $FUNCTION_APP_NAME"
